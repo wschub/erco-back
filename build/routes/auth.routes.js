9ef12577e43505bea3fb6dbd2f1d88d989cb4d3f
wschub <dev.wschub@gmail.com>
2025-05-18 10:18:28 -0500

    omit build folder

--- build/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = (0, express_1.Router)();
-router.post('/register', auth_controller_1.register); // {email, password, role}
-router.post('/login', auth_controller_1.login); // {email, password}
-exports.default = router;
